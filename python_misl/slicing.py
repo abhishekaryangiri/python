2e9e2ea2b72966f7a27142995dd91a520dd166d8
Abhishek <abhishek.aryangiri@gmail.com>
2026-06-29 23:43:16 +0530

    list

---
+++ python_misl/slicing.py
@@ -0,0 +1,9 @@
+list2 = ["Mango", "Banana",100,10.5,True]
+list3 = ["Mango", "Banana",100,10.5,True]
+list4 = ["Mango", "Banana",100,10.5,True]
+
+# Negative index from end -num: or :num-
+print(list2[-2:])
+
+# -ve index from starting
+#print(list3[:-1])
